703d6a4548e2206d02ae3efa4dc8523d8034ab09
QueeneyZ <QueeneyZ@outlook.com>
2025-06-20 11:08:26 +0000

    [automatic commit] 2025-06-20 11:08:25 UTC 129.127.202.4

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -41,6 +41,7 @@ router.get('/walkers/summary', async (req, res) => {
             LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
             LEFT JOIN WalkRequests wr ON r.request_id = wr.request_id AND wr.status = 'completed'
             WHERE u.role = 'walker'
-            `)
+            GROUP BY u.username
+        `);
     }
 })
