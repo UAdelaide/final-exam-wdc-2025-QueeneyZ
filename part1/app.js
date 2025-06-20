bce00d64b34b032da65d66c4386096a3c0cfbd1c
QueeneyZ <QueeneyZ@outlook.com>
2025-06-20 11:32:12 +0000

    [automatic commit] 2025-06-20 11:32:12 UTC 129.127.202.4

--- part1/app.js
+++ part1/app.js
@@ -16,6 +16,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', requeire)
+app.use('/api', require('./routes/api'));
 
 module.exports = app;
