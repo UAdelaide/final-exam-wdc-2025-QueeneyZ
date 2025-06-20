1ab83e8fc8fff59544397cdeebe8fac17d9c10ef
QueeneyZ <QueeneyZ@outlook.com>
2025-06-20 10:55:11 +0000

    [automatic commit] 2025-06-20 10:55:11 UTC 129.127.202.4

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/dogs', async (requestAnimationFrame, res) =>)
+router.get('/dogs', async (req, res) => {
+    try {
+        const []
+    }
+})
