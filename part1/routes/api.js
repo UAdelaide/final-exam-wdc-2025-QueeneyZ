6caff039a8ba1c585ee3e8f2a8831bd63a2018c4
QueeneyZ <QueeneyZ@outlook.com>
2025-06-20 11:00:54 +0000

    [automatic commit] 2025-06-20 11:00:53 UTC 129.127.202.4

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -18,6 +18,8 @@ router.get('walkrequests/open', async (req, res) => {
     try {
         const [rows] = await db.execute(`
             SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.request_time,
-                   WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner`)
+                   WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
+            FROM WalkRequests
+            `)
     }
 })
