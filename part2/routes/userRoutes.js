3b62883eb558f984ad61fab87bf159fb08c2cb7a
QueeneyZ <QueeneyZ@outlook.com>
2025-06-20 13:21:22 +0000

    [automatic commit] 2025-06-20 13:21:22 UTC 129.127.202.4

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,7 +65,7 @@ router.post('/login', async (req, res) => {
     return res.status(500).send('Login failed');
   }
  });
- console.log('Attempted login:', username, password);
+
 
 
  router.get('/logout', (req, res) => {
