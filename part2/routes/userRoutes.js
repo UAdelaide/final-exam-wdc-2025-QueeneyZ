c0b21341b5dd14ea63e45b04c3edd867c2f59503
QueeneyZ <QueeneyZ@outlook.com>
2025-06-20 12:38:43 +0000

    [automatic commit] 2025-06-20 12:38:43 UTC 129.127.202.4

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -46,7 +46,7 @@ router.post('/login', async (req, res) => {
     `, [email, password]);
 
     if (rows.length === 0) {
-      return res.status(401).send('Invalid credentials');
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
 
     res.json({ message: 'Login successful', user: rows[0] });
