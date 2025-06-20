aa42dfd115af9e6aefe414ab5836f32d8fd6f34e
QueeneyZ <QueeneyZ@outlook.com>
2025-06-20 14:14:45 +0000

    [automatic commit] 2025-06-20 14:14:45 UTC 129.127.202.4

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -73,7 +73,7 @@ router.post('/login', async (req, res) => {
 
   try {
     const [rows] = await db.query(
-      'SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
+      'SELECT dog_id, name, size FROM Dogs WHERE owner_id = ?',
       [req.session.user.user_id]
     );
     res.json(rows);
